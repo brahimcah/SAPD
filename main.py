d7ba70d85967c3bce3b5d16131be331adf99fb54
Brahim Chahbouni <brahimcah@gmail.com>
2023-04-03 10:39:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import zipfile
 
 
 
-def open_window1():
-    # add widgets to window1
-    system("del mail")
-    system("curl -O https://api.ian-service.com/dgt/mail.ini")
-    system("ren mail.ini mail")
-
 def open_window2():
     system("del permisos.csv")
     system("del mail.txt")
@@ -33,9 +27,7 @@ main_window.title('SAPD')
 main_window.geometry('500x500')
 
 # create buttons
-button1 = tk.Button(main_window, text='Actualizar Mail Envio', command=open_window1)
 button2 = tk.Button(main_window, text='Generar Avisos de Permisos DGT', command=open_window2)
-button3 = tk.Button(main_window, text='Actualizar Aplicación', command=open_window3)
 
 # add buttons to main window
 button1.pack()
