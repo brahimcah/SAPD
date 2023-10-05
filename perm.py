b778825e62fa1a693fbad32c602e15d750ef1eff
Usuario Administrador de BRAHIM CHAHBOUNI-AMP <brahimcah@gmail.com>
2023-10-05 10:32:57 +0200

    Actualización de la Solicitud
    Se añade en el formulario de solicitud una opción que sirve para que el usuario indique la fecha de naciemiento

--- perm.py
+++ perm.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import urllib.parse
 
 def generar_url_mailto():
@@ -6,57 +7,66 @@ def generar_url_mailto():
     nombre = nombre_entry.get()
     num_centro = num_centro_entry.get()
     doi = doi_entry.get()
-    tipo_centro = tipo_centro_var.get()
     buzon = buzon_entry.get()
+    tipo_centro = tipo_centro_var.get()
+    year_of_birth = year_entry.get()
 
-    # Crear el mensaje de correo electrónico 
-    #El GI-000 ha sido informado por el SAPD que el permiso de conducir DNI/NIE se encuentra en la Jefatura con el número 000
-    mensaje = f"Buenos días,\n El {tipo_centro} ha sido informado por el SAPD que el permiso de conducir {doi} se encuentra en la Jefatura con el número {buzon}\nNombre: {nombre}\nNº CENTRO: {num_centro}\nDOI: {doi}\nTipo de Centro: {tipo_centro}"
-    
-    # Codificar el mensaje y el asunto como parámetros en una URL "mailto"
-    body = urllib.parse.quote(mensaje)
-    asunto = urllib.parse.quote(f"RECOGIDA PERMISO {nombre} ({doi})")
-    mailto_url = f"mailto:crc.jptgi@dgt.es?subject={asunto}&body={body}"
+    # Validar que todos los campos estén llenos
+    if nombre and num_centro and doi and buzon and tipo_centro != "-" and year_of_birth:
+        # Crear el mensaje de correo electrónico 
+        mensaje = f"Buenos días,\nEl {tipo_centro} ha sido informado por el SAPD que el permiso de conducir {doi} se encuentra en la Jefatura con el número {buzon}\nNombre: {nombre}\nNº CENTRO: {num_centro}\nDOI: {doi}\nAño de Nacimiento: {year_of_birth}\nTipo de Centro: {tipo_centro}"
+        
+        # Codificar el mensaje y el asunto como parámetros en una URL "mailto"
+        body = urllib.parse.quote(mensaje)
+        asunto = urllib.parse.quote(f"RECOGIDA PERMISO {nombre} ({doi})")
+        mailto_url = f"mailto:crc.jptgi@dgt.es?subject={asunto}&body={body}"
 
-    # Abrir la URL "mailto" en el navegador por defecto
-    import webbrowser
-    webbrowser.open(mailto_url)
+        # Abrir la URL "mailto" en el navegador por defecto
+        import webbrowser
+        webbrowser.open(mailto_url)
+    else:
+        # Mostrar un mensaje de error si algún campo está vacío
+        messagebox.showerror("Error", "Por favor, complete todos los campos del formulario.")
 
 # Crear la ventana principal
 root = tk.Tk()
+root.title("Generador de URL de Correo Electrónico")
 
-# Crear los campos del formulario
+# Crear y organizar los campos del formulario usando grid
 nombre_label = tk.Label(root, text="Nombre:")
-nombre_label.pack()
+nombre_label.grid(row=0, column=0, sticky="w")
 nombre_entry = tk.Entry(root)
-nombre_entry.pack()
+nombre_entry.grid(row=0, column=1)
 
 num_centro_label = tk.Label(root, text="Nº CENTRO:")
-num_centro_label.pack()
+num_centro_label.grid(row=1, column=0, sticky="w")
 num_centro_entry = tk.Entry(root)
-num_centro_entry.pack()
+num_centro_entry.grid(row=1, column=1)
 
 doi_label = tk.Label(root, text="DOI:")
-doi_label.pack()
+doi_label.grid(row=2, column=0, sticky="w")
 doi_entry = tk.Entry(root)
-doi_entry.pack()
+doi_entry.grid(row=2, column=1)
 
-buzons = tk.Label(root, text="Buzon:")
-buzons.pack()
+buzon_label = tk.Label(root, text="Buzon:")
+buzon_label.grid(row=3, column=0, sticky="w")
 buzon_entry = tk.Entry(root)
-buzon_entry.pack()
+buzon_entry.grid(row=3, column=1)
 
-# Crear la opción de selección del tipo de centro
+year_label = tk.Label(root, text="Año de Nacimiento:")
+year_label.grid(row=4, column=0, sticky="w")
+year_entry = tk.Entry(root)
+year_entry.grid(row=4, column=1)
+
 tipo_centro_label = tk.Label(root, text="Tipo de Centro:")
-tipo_centro_label.pack()
+tipo_centro_label.grid(row=5, column=0, sticky="w")
 tipo_centro_var = tk.StringVar()
 tipo_centro_var.set("-")
 tipo_centro_menu = tk.OptionMenu(root, tipo_centro_var, "Centro Médico", "Autoescuela", "Gestoría")
-tipo_centro_menu.pack()
+tipo_centro_menu.grid(row=5, column=1, sticky="w")
 
-# Crear el botón para generar la URL "mailto"
 generar_url_boton = tk.Button(root, text="Generar URL", command=generar_url_mailto)
-generar_url_boton.pack()
+generar_url_boton.grid(row=6, column=0, columnspan=2, pady=10)
 
 # Iniciar la ventana principal
 root.mainloop()
