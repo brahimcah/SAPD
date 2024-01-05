127371c5d9decbae27362ad0cea088c4d5560e0a
Usuario Administrador de BRAHIM CHAHBOUNI-AMP <brahimcah@gmail.com>
2024-01-05 10:09:56 +0100

    Update perm.py

--- perm.py
+++ perm.py
@@ -8,13 +8,12 @@ def generar_url_mailto():
     num_centro = num_centro_entry.get()
     doi = doi_entry.get()
     buzon = buzon_entry.get()
-     
     year_of_birth = year_entry.get()
 
     # Validar que todos los campos estén llenos
     if nombre and num_centro and doi and buzon and year_of_birth:
         # Crear el mensaje de correo electrónico 
-        mensaje = f"Buenos días,\n hemos sido informado por el SAPD que el permiso de conducir con DOI: {doi} se encuentra en la Jefatura con el número {buzon}\nNombre: {nombre}\nNº CENTRO: {num_centro}\nDOI: {doi}\nAño de Nacimiento: {year_of_birth}\n Un Saludo"
+        mensaje = f"Buenos días,\n hemos sido informado por el SAPD que el permiso de conducir con DOI: {doi} se encuentra en la Jefatura con el número {buzon}\n\nNombre y Apellidos: {nombre}\nNº CENTRO: {num_centro}\nDOI: {doi}\n Fecha de Nacimiento: {year_of_birth}\n Un Saludo"
         
         # Codificar el mensaje y el asunto como parámetros en una URL "mailto"
         body = urllib.parse.quote(mensaje)
@@ -33,34 +32,39 @@ root = tk.Tk()
 root.title("Generador de URL de Correo Electrónico")
 
 # Crear y organizar los campos del formulario usando grid
-nombre_label = tk.Label(root, text="Nombre y Apellidos:")
-nombre_label.grid(row=0, column=0, sticky="w")
+campos = [
+    ("Nombre y Apellidos:", 0, 0),
+    ("Nº CENTRO:", 1, 0),
+    ("DOI:", 2, 0),
+    ("Buzon:", 3, 0),
+    ("Fecha de Nacimiento:", 4, 0)
+]
+
+for label_text, row, col in campos:
+    label = tk.Label(root, text=label_text)
+    label.grid(row=row, column=col, sticky="w", padx=10, pady=5)
+
 nombre_entry = tk.Entry(root)
-nombre_entry.grid(row=0, column=1)
+nombre_entry.grid(row=0, column=1, padx=10, pady=5)
 
-num_centro_label = tk.Label(root, text="Nº CENTRO:")
-num_centro_label.grid(row=1, column=0, sticky="w")
 num_centro_entry = tk.Entry(root)
-num_centro_entry.grid(row=1, column=1)
+num_centro_entry.grid(row=1, column=1, padx=10, pady=5)
 
-doi_label = tk.Label(root, text="DOI:")
-doi_label.grid(row=2, column=0, sticky="w")
 doi_entry = tk.Entry(root)
-doi_entry.grid(row=2, column=1)
+doi_entry.grid(row=2, column=1, padx=10, pady=5)
 
-buzon_label = tk.Label(root, text="Buzon:")
-buzon_label.grid(row=3, column=0, sticky="w")
 buzon_entry = tk.Entry(root)
-buzon_entry.grid(row=3, column=1)
+buzon_entry.grid(row=3, column=1, padx=10, pady=5)
 
-year_label = tk.Label(root, text="Fecha de Nacimiento:")
-year_label.grid(row=4, column=0, sticky="w")
 year_entry = tk.Entry(root)
-year_entry.grid(row=4, column=1)
-
+year_entry.grid(row=4, column=1, padx=10, pady=5)
 
 generar_url_boton = tk.Button(root, text="Generar URL", command=generar_url_mailto)
-generar_url_boton.grid(row=6, column=0, columnspan=2, pady=10)
+generar_url_boton.grid(row=5, column=0, columnspan=2, pady=10)
+
+# Ajustar el tamaño de las columnas
+for i in range(2):
+    root.grid_columnconfigure(i, weight=1)
 
 # Iniciar la ventana principal
 root.mainloop()
