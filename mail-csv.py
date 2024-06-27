f92299dc182a345f865e24ef9e8b19dc57bf07e5
Brahim Chahbouni <brahimcah@gmail.com>
2024-06-27 21:45:37 +0200

    Update mail-csv.py

--- mail-csv.py
+++ mail-csv.py
@@ -1,30 +1,31 @@
 import tkinter as tk
 import subprocess
-from os import system
 
 class Menu:
     def __init__(self, master):
         self.master = master
         master.title("SAPD")
         master.geometry("500x620")  # Define el tamaño de la ventana principal
+        master.configure(bg="#f0f0f0")  # Fondo de la ventana principal
 
         # Colores
-        bg_color = "#333"
-        label_fg_color = "#fff"
-        button_fg_color = "#fff"
+        bg_color = "#f0f0f0"
+        frame_color = "#ffffff"
+        label_fg_color = "#333333"
+        button_fg_color = "#ffffff"
         button_bg_color = {
-            "a": "#118de6",
-            "b": "#30a41b",
-            "c": "#ff0808",
-            "d": "#5dc1b9",
-            "salir": bg_color,
+            "a": "#007bff",
+            "b": "#28a745",
+            "c": "#dc3545",
+            "d": "#17a2b8",
+            "salir": "#6c757d",
         }
 
-        self.frame = tk.Frame(master, bg=bg_color)
-        self.frame.pack(expand=True, fill="both")
+        self.frame = tk.Frame(master, bg=frame_color, bd=2, relief="groove")
+        self.frame.pack(expand=True, fill="both", padx=20, pady=20)
 
         # Crear etiqueta de selección
-        self.label = tk.Label(self.frame, text="SISTEMA AUTOMATIZADO \n DE PERMISOS DEVUELTOS ", fg=label_fg_color, bg=bg_color, font=("Helvetica", 16))
+        self.label = tk.Label(self.frame, text="SISTEMA AUTOMATIZADO\nDE PERMISOS DEVUELTOS", fg=label_fg_color, bg=frame_color, font=("Helvetica", 16, "bold"))
         self.label.pack(pady=30)
 
         # Botones en un diccionario para facilitar su creación y gestión
@@ -38,11 +39,11 @@ class Menu:
 
         for key, (text, width, height, command) in buttons.items():
             button = tk.Button(self.frame, text=text, fg=button_fg_color, bg=button_bg_color.get(key, None),
-                               font=("Helvetica", 12), width=width, height=height, command=command)
+                               font=("Helvetica", 12), width=width, height=height, command=command, bd=0, relief="solid")
             button.pack(pady=10)
 
         # Etiqueta de versión
-        self.version_label = tk.Label(self.frame, text="Versión: 4.2.1 - 02/01/2024", fg=label_fg_color, bg=bg_color, font=("Helvetica", 11))
+        self.version_label = tk.Label(self.frame, text="Versión: 4.2.1 - 02/01/2024", fg=label_fg_color, bg=frame_color, font=("Helvetica", 11))
         self.version_label.pack(pady=20)
 
     def ejecutar_a(self):
@@ -50,7 +51,6 @@ class Menu:
 
     def ejecutar_b(self):
         subprocess.run(['python3', 'perm.py'])
-
 
     def ejecutar_c(self):
         subprocess.run(['python3', 'soporte.py'])
